9f02a1129e41179317e666f8b49094225b7b2847
danmcCs111 <danielboydelatour@gmail.com>
2025-02-24 02:24:27 -0500

    add printing of output

--- commons-lang3-TRUNK/src/main/java/app/hello.java
+++ commons-lang3-TRUNK/src/main/java/app/hello.java
@@ -2,11 +2,13 @@ package app;
 
 import java.sql.Date;
 import java.util.Calendar;
+import java.util.List;
 
 import org.apache.commons.commons_lang3.HibernateUtil;
 import org.apache.commons.commons_lang3.Video;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
 
 public class hello 
 {
@@ -25,12 +27,19 @@ public class hello
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		//start transaction
-//		session.beginTransaction();
+		session.beginTransaction();
 //		session.create
 		//Save the Model object
-//		session.save(video);
+		NativeQuery nq = session.createSQLQuery("select * from videodatabase.Video");
+		List l = nq.getResultList();
+		for(int i =0; i < l.size(); i++)
+		{
+			Object [] os = (Object[]) l.get(i);
+			for(Object o : os)
+				System.out.println("output - > " + o.toString());
+		}
 		//Commit transaction
-//		session.getTransaction().commit();
+		session.getTransaction().commit();
 		System.out.println("Employee ID=" + video.getVideoId());
 		
 		//terminate session factory, otherwise program won't end
