3548a040f589eea6f06a8c635913750106a23ce8
danmcCs111 <danmccs111@gmail.com>
2025-03-05 15:44:58 -0500

    forgot to update columnnames with '_' seperator convension

--- DriverAdapter/module-info.java
+++ DriverAdapter/module-info.java
@@ -1,9 +1,6 @@
 /**
  * 
  */
-/**
- * 
- */
 module DriverAdapter {
 	requires java.sql;
 }
--- DriverAdapter/src/DriverAdapter/VideoSelect.java
+++ DriverAdapter/src/DriverAdapter/VideoSelect.java
@@ -12,11 +12,11 @@ public class VideoSelect
 	public static final String SELECT_SQL = "select * from videodatabase.video";
 	public static ArrayList<Holder> videoMap = new ArrayList<Holder>();
 	static {
-		videoMap.add(new IntegerHolder("VideoIdVideoVideoDatabase"));
-	    videoMap.add(new StringHolder("VideoNameVideoVideoDatabase"));
-	    videoMap.add(new StringHolder("VideoPathVideoVideoDatabase"));
-	    videoMap.add(new StringHolder("VideoExtensionVideoVideoDatabase"));
-	    videoMap.add(new DateHolder("InsertDateVideoVideoDatabase"));
+		videoMap.add(new IntegerHolder("VideoId_Video_VideoDatabase"));
+	    videoMap.add(new StringHolder("VideoName_Video_VideoDatabase"));
+	    videoMap.add(new StringHolder("VideoPath_Video_VideoDatabase"));
+	    videoMap.add(new StringHolder("VideoExtension_Video_VideoDatabase"));
+	    videoMap.add(new DateHolder("InsertDate_Video_VideoDatabase"));
 	}
 	
 	public static ArrayList<Holder> getHolders()
