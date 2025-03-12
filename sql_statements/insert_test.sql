b44e788b09ddf6cc237765cf3c1ccc29dca35e5b
danmcCs111 <danmccs111@gmail.com>
2025-03-12 16:42:28 -0400

    add webservice query tables and webservice type code table

--- sql_statements/insert_test.sql
+++ sql_statements/insert_test.sql
@@ -24,3 +24,45 @@ commit;
 SHOW TABLES FROM videodatabase;
 
 SHOW COLUMNS FROM videodatabase.Video;
+
+CREATE TABLE videodatabase.Webservice (
+    WebserviceId_Webservice_VideoDatabase int PRIMARY KEY,
+    WebserviceName_Webservice_VideoDatabase VARCHAR(300),
+    WebserviceQuery_Webservice_VideoDatabase VARCHAR(5000),
+    WebserviceType_Webservice_VideoDatabase int,
+    InsertDate_Webservice_VideoDatabase timestamp
+);
+
+insert into videodatabase.Webservice
+	(
+    WebserviceId_Webservice_VideoDatabase,
+    WebserviceName_Webservice_VideoDatabase,
+    WebserviceQuery_Webservice_VideoDatabase,
+    WebserviceType_Webservice_VideoDatabase,
+    InsertDate_Webservice_VideoDatabase
+    )
+values (
+	1,
+    "SelectAllVideos",
+    "Select * from videoDatabase.Video",
+    1,
+    (select current_timestamp())
+);
+
+CREATE TABLE videodatabase.WebserviceType (
+    WebserviceTypeId_Webservice_VideoDatabase int PRIMARY KEY,
+    WebserviceTypeName_Webservice_VideoDatabase VARCHAR(300),
+    InsertDate_Webservice_VideoDatabase timestamp
+);
+
+insert into videodatabase.WebserviceType
+	(
+	WebserviceTypeId_Webservice_VideoDatabase,
+    WebserviceTypeName_Webservice_VideoDatabase,
+    InsertDate_Webservice_VideoDatabase
+    )
+values (
+	1,
+    "User Query",
+    (select current_timestamp())
+);
