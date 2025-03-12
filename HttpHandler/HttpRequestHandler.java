33f2b377f51840a5f31748c8bfe70532ab3c5a2a
danmcCs111 <danmccs111@gmail.com>
2025-03-12 08:46:06 -0400

    add relaying of headers back in response

--- HttpHandler/HttpRequestHandler.java
+++ HttpHandler/HttpRequestHandler.java
@@ -2,7 +2,9 @@ package HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -11,11 +13,18 @@ public class HttpRequestHandler implements HttpHandler
 	@Override
 	public void handle(HttpExchange exchange) throws IOException 
 	{
-		 String response = "This is the response";
-		 exchange.sendResponseHeaders(200, response.length());
-         OutputStream os = exchange.getResponseBody();
-         os.write(response.getBytes());
-         os.close();
+		Headers h = exchange.getRequestHeaders();
+		String response = "This is the response " + "\n";
+		for(String key : h.keySet())
+		{
+			List<String> headers = h.get(key);
+			for(String s : headers)
+				response += s + "\n";
+		}
+		exchange.sendResponseHeaders(200, response.length());
+		OutputStream os = exchange.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
 	}
 
 }
