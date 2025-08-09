a9985d35098ab0843186f6e7c0ce4e64804cbecb
danmcCs111 <danmccs111@gmail.com>
2025-08-09 16:22:52 -0400

    update sql

--- sql_statements/weather_sql.sql
+++ sql_statements/weather_sql.sql
@@ -4,22 +4,21 @@ create database WeatherDatabase;
 
 CREATE TABLE WeatherDatabase.WeatherReading
 (
-        WeatherDate_WeatherReading_WeatherDatabase timestamp PRIMARY KEY,
-        WeatherTemperature_WeatherReading_WeatherDatabase int,
-        WeatherRain_WeatherReading_WeatherDatabase int,
-        WeatherThunder_WeatherReading_WeatherDatabase int,
-        WeatherPrecipitationPotential_WeatherReading_WeatherDatabase int,
-        WeatherRelativeHumidity_WeatherReading_WeatherDatabase int,
-        WeatherSkyCover_WeatherReading_WeatherDatabase int,
-        WeatherHeatIndex_WeatherReading_WeatherDatabase int,
-        WeatherDewpoint_WeatherReading_WeatherDatabase int,
+        Date_WeatherReading_WeatherDatabase timestamp PRIMARY KEY,
+        Temperature_WeatherReading_WeatherDatabase int,
+        Rain_WeatherReading_WeatherDatabase int,
+        Thunder_WeatherReading_WeatherDatabase int,
+        PrecipitationPotential_WeatherReading_WeatherDatabase int,
+        RelativeHumidity_WeatherReading_WeatherDatabase int,
+        SkyCover_WeatherReading_WeatherDatabase int,
+        HeatIndex_WeatherReading_WeatherDatabase int,
+        Dewpoint_WeatherReading_WeatherDatabase int,
         InsertDate_WeatherReading_WeatherDatabase timestamp
 );
 
 select * from WeatherDatabase.WeatherReading;
 
 commit;
-
 
 SHOW TABLES FROM WeatherDatabase;
 
