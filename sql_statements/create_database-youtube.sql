3e8983d3040e18a6185ef9e7e4c7dbe3e705e5f1
danmcCs111 <danmccs111@gmail.com>
2026-01-01 14:28:54 -0500

    update table def

--- sql_statements/create_database-youtube.sql
+++ sql_statements/create_database-youtube.sql
@@ -4,12 +4,11 @@ commit;
 use videodatabase;
 
 CREATE TABLE videodatabase.videoYoutube (
-	Id_VideoYoutube_VideoYoutubeDatabase int NOT NULL AUTO_INCREMENT,
+	Id_VideoYoutube_VideoYoutubeDatabase VARCHAR(15),
     ParentID_VideoYoutube_VideoYoutubeDatabase int NOT NULL,
     Title_VideoYoutube_VideoYoutubeDatabase VARCHAR(300),
     Url_VideoYoutube_VideoYoutubeDatabase VARCHAR(500),
 	PosterImageUrl_VideoYoutube_VideoYoutubeDatabase VARCHAR(500),
-    PosterImage_VideoYoutube_VideoYoutubeDatabase BLOB,
 	UploadDate_VideoYoutube_VideoYoutubeDatabase timestamp,
     InsertDate_VideoYoutube_VideoYoutubeDatabase timestamp,
     PRIMARY KEY (Id_VideoYoutube_VideoYoutubeDatabase)
