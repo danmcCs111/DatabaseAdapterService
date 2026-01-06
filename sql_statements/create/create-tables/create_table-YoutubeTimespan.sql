7d7253d229ae552e893075ea5f7404123f923450
danmcCs111 <danmccs111@gmail.com>
2026-01-06 15:02:22 -0500

    correct typo on column names

--- sql_statements/create/create-tables/create_table-YoutubeTimespan.sql
+++ sql_statements/create/create-tables/create_table-YoutubeTimespan.sql
@@ -1,11 +1,11 @@
 use videodatabase;
 
 CREATE TABLE videodatabase.YoutubeTimespan (
-    ParentID_VideoYoutube_YoutubeTimespan int NOT NULL,
-	TimespanDefaultMillis_VideoYoutube_YoutubeTimespan long default (2629746000 * 6), #month 2629746000
-	LookupCountPerDay_VideoYoutube_YoutubeTimespan int default (5), #default 5 per day
-    InsertDate_VideoYoutube_YoutubeTimespan timestamp,
-    PRIMARY KEY (ParentID_VideoYoutube_YoutubeTimespan)
+    ParentID_YoutubeTimespan_VideoDatabase int NOT NULL,
+	TimespanDefaultMillis_YoutubeTimespan_VideoDatabase long default (2629746000 * 6), #month 2629746000
+	LookupCountPerDay_YoutubeTimespan_VideoDatabase int default (5), #default 5 per day
+    InsertDate_YoutubeTimespan_VideoDatabase timestamp,
+    PRIMARY KEY (ParentID_YoutubeTimespan_VideoDatabase)
 );
 
 commit;
