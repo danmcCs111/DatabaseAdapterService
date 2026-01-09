ef0c275e2711f83de4b931dccfef9f2820c54595
danmcCs111 <danmccs111@gmail.com>
2026-01-08 21:58:05 -0500

    add create table sql

---
+++ sql_statements/create/create-tables/create_table-videodatabase-queries.sql
@@ -0,0 +1,8 @@
+CREATE TABLE VideoDatabase.Webservice 
+(
+	WebserviceId_Webservice_VideoDatabase int PRIMARY KEY,
+	WebserviceName_Webservice_VideoDatabase VARCHAR(300),
+	WebserviceQuery_Webservice_VideoDatabase VARCHAR(5000),
+	WebserviceType_Webservice_VideoDatabase int,
+	InsertDate_Webservice_VideoDatabase timestamp
+);
