607446d38def9fd2f2073e912132cdd44d2c0e1a
danmcCs111 <danmccs111@gmail.com>
2026-01-11 10:12:47 -0500

    update to read from sqlite database

--- echoArgs.sh
+++ echoArgs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 dbUrl=`egrep -o DB_URL.* config/db-config.txt | sed 's/DB_URL=//g'`
+dbUrlPre=`egrep -o DB_URL_SQLITE_PRE.* config/db-config.txt | sed 's/DB_URL_SQLITE_PRE=//g'`
+dbUrlPost=`egrep -o DB_URL_SQLITE_POST.* config/db-config.txt | sed 's/DB_URL_SQLITE_POST=//g'`
 user=`egrep -o USER.* config/db-config.txt | sed 's/USER=//g'`
 pass=`egrep -o PASS.* config/db-config.txt | sed 's/PASS=//g'`
 port=`egrep -o PORT_NUMBER.* config/db-config.txt | sed 's/PORT_NUMBER=//g'`
--- runDatabase.sh
+++ runDatabase.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+db_file="$1"
 source ./echoArgs.sh
 source ./java-class-path.sh
-java -cp "$java_database_cp" DriverAdapter.DriverAdapter $dbUrl $user $pass $port
+
+#mysql
+#java -cp "$java_database_cp" DriverAdapter.DriverAdapter $dbUrl $user $pass $port
+
+java -cp "$java_database_cp" DriverAdapter.DriverAdapter $dbUrlPre$db_file$dbUrlPost $port
 
