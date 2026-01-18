063ed06884ef46b4a1885b12e7315b26cd9aeca7
danmcCs111 <danmccs111@gmail.com>
2026-01-18 17:48:59 -0500

    adjust create table

--- sql_statements/create/create-tables/create_table-game.sql
+++ sql_statements/create/create-tables/create_table-game.sql
@@ -1,14 +1,13 @@
 use gamedatabase;
 
 CREATE TABLE gamedatabase.Game (
-    GameId_Game_GameDatabase int NOT NULL AUTO_INCREMENT,
     GameTitle_Game_GameDatabase VARCHAR(300),
     GameUrl_Game_GameDatabase VARCHAR(500),
     GameBaseValue_Game_GameDatabase float,
     GameFinalValue_Game_GameDatabase float,
     GamePosterImage_Game_GameDatabase BLOB,
     InsertDate_Game_GameDatabase timestamp,
-    PRIMARY KEY (GameId_Game_GameDatabase)
+    PRIMARY KEY (GameTitle_Game_GameDatabase)
 );
 
 commit;
