8daa41cdf476fb470cec244b27e49732cc747b08
danmcCs111 <danmccs111@gmail.com>
2026-01-18 23:29:43 -0500

    add sqls

--- sql_statements/create/create-tables/create_table-GameCost - sqlite.sql
+++ sql_statements/create/create-tables/create_table-GameCost - sqlite.sql
@@ -5,6 +5,7 @@ CREATE TABLE gamedatabase.Game (
     GameUrl_Game_GameDatabase VARCHAR(500),
     GameBaseValue_Game_GameDatabase float,
     GameFinalValue_Game_GameDatabase float,
+    GamePosterImage_Game_GameDatabase BLOB,
     InsertDate_Game_GameDatabase timestamp,
     PRIMARY KEY (GameTitle_Game_GameDatabase)
 );
--- sql_statements/create/create-tables/create_table-game - sqlite.sql
+++ sql_statements/create/create-tables/create_table-game - sqlite.sql
@@ -2,7 +2,6 @@ use gamedatabase;
 
 CREATE TABLE gamedatabase.Game (
     GameTitle_Game_GameDatabase VARCHAR(300),
-    GameUrl_Game_GameDatabase VARCHAR(500),
     GameBaseValue_Game_GameDatabase float,
     GameFinalValue_Game_GameDatabase float,
     InsertDate_Game_GameDatabase timestamp,
