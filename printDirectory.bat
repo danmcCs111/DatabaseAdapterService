367b28ee0c5ce70d0fb9531c65ebaf2f256583a9
danmcCs111@gmail.com <danmcCs111@gmail.com>
2026-02-20 15:57:48 -0500

    remove old bat file

--- printDirectory.bat
+++
@@ -1,5 +0,0 @@
-set orig=%CD%
-cd ../SQLiteInstall
-set new=%CD%
-cd %orig%
-echo %new% > dirArg.txt
--- runDatabase.sh
+++ runDatabase.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-./printDirectory.bat
-db_file=`cat dirArg.txt | sed 's/ $//g'`"\\"
 source ./echoArgs.sh
 source ./java-class-path.sh
 
-echo $db_file
 #mysql
 #java -cp "$java_database_cp" DriverAdapter.DriverAdapter $dbUrl $user $pass $port
 
